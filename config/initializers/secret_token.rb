8991bc2581fda3c53be2c5693d17ac0812a0760c
Cuong Bui <bpcuong2013@gmail.com>
2013-09-04 03:30:34 +0700

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListsofLists::Application.config.secret_token = '5022310d925a63ce3adc2ff01d5306deea7482aebb499a6e215ce2ffc50d07ed7608147d733daba47d2c42d634b265be9ba06fc22c05a41b56973449e3903add'
