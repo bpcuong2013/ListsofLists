cbc5a21f65d7b1102513c42fc99c8194e42b99e2
thanh <thanh@gogocloudzilla.com>
2013-09-23 23:55:43 +0700

    add facebook, google keys for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :facebook, '644473238897361', '433f4574a7cc28dd670886b4c5175dc6'  
-  provider :google_oauth2, '1027665096433.apps.googleusercontent.com','VC0aEfQMINad1wNNSTEYq9YB'
+  provider :facebook, '665261240158184', 'f2885dd99652ad778c9543ef7cd763de'  
+  provider :google_oauth2, '361914808375.apps.googleusercontent.com','hU4cfF0Xwpm4XqrvsAKFfagn'
   provider :twitter, 'TTNBI2EFPDb8M4TkZMww', 'kjQO578wIagDNVzdDex9j1nb1Ipk1bV9FXQRbk4hRc'
 end
