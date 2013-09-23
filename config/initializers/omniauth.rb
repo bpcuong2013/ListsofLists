01df324dad574880c7a20fd0e53cdbc52445fe95
thanh <thanh@gogocloudzilla.com>
2013-09-24 00:22:35 +0700

    twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   provider :facebook, '665261240158184', 'f2885dd99652ad778c9543ef7cd763de'  
   provider :google_oauth2, '361914808375.apps.googleusercontent.com','hU4cfF0Xwpm4XqrvsAKFfagn'
-  provider :twitter, 'TTNBI2EFPDb8M4TkZMww', 'kjQO578wIagDNVzdDex9j1nb1Ipk1bV9FXQRbk4hRc'
+  provider :twitter, 'r9BKERwHHyGSkuZeKbey6Q', '2PBnJBtlK0bTNpMJRBAuDAq2kaG3e0P9QaW0mjeLyQ'
 end
